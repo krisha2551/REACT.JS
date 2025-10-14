dee9cb91eefaad95a22ee5a64e8dec69c963203e
krisha2551 <krishajasani62@gmail.com>
2025-10-14 18:39:26 +0530

    Add files via upload

--- Hooks/src/App.jsx
+++ Hooks/src/App.jsx
@@ -1,25 +1,47 @@
-import OnChange from "./Events/OnChange";
-import OnClick from "./Events/OnClick";
-import OnSubmit from "./Events/OnSubmit";
-import LifeCycle from "./Hooks/LifeCycle";
-import ObjectUseState from "./Hooks/ObjectUseState";
-import UseEffect from "./Hooks/UseEffect";
-import UseState from "./Hooks/UseState";
+ import OnChange from "./Events/OnChange";
+ import OnClick from "./Events/OnClick";
+ import OnSubmit from "./Events/OnSubmit";
+ import PassingArguments from "./Events/PassingArguments";
+ import Conditional from "./Events/Conditional";
 
-const App = () => {
-  return (
-    <>
-      {/* <UseState /> */}
-      {/* <ObjectUseState/> */}
+ import LifeCycle from "./Hooks/LifeCycle";
+ import ObjectUseState from "./Hooks/ObjectUseState";
+ import UseEffect from "./Hooks/UseEffect";
+ import UseState from "./Hooks/UseState";
+
+ import List from "./Lists/List";
+ import ObjectList from "./Lists/ObjectList";
+ import Focus from "./Refs/Focus";
+
+
+ const App = () => {
+   return (
+     <>
+       {/* <UseState /> */}
+       {/* <ObjectUseState/> */}
       {/* <UseEffect/> */}
-      {/* <LifeCycle/> */}
+       {/* <LifeCycle/> */}
       
       {/* <OnChange/> */}
-      {/* <OnClick/> */}
-      <OnSubmit/>
+       {/* <OnClick/> */}
+       {/* <OnSubmit/> */}
+          
+      {/* <PassingArguments propName="Charlie" /> */}
+      {/* <Conditional/> */}
+
+      {/* lists */}
+
+      {/* <List /> */}
+
+      {/* <ObjectList /> */}
+
+      {/* ref */}
+
+      {/* <Focus /> */}
+
     </>
-  );
-};
+   );
+ };
 
 export default App;
 
