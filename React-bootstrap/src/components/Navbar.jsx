8256a005a67e6f0b30f0e3e48ca951a125fd3a38
krisha2551 <krishajasani62@gmail.com>
2025-11-20 17:09:19 +0530

    Add files via upload

--- React-bootstrap/src/components/Navbar.jsx
+++ React-bootstrap/src/components/Navbar.jsx
@@ -1,15 +1,17 @@
 import React from "react";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
-import RBNavbar from "react-bootstrap/Navbar";
+import Navbar from "react-bootstrap/Navbar"; // ← Direct import (no rename)
 
 function NavbarMenu() {
   return (
-    <RBNavbar expand="lg" className="bg-body-tertiary" sticky="top">
+    <Navbar expand="lg" className="bg-body-tertiary" sticky="top">
       <Container>
-        <RBNavbar.Brand href="#home">Wanderlust</RBNavbar.Brand>
-        <RBNavbar.Toggle aria-controls="basic-navbar-nav" />
-        <RBNavbar.Collapse id="basic-navbar-nav">
+        <Navbar.Brand href="#home">Wanderlust</Navbar.Brand>
+
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+
+        <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ms-auto">
             <Nav.Link href="#home">Home</Nav.Link>
             <Nav.Link href="#about">About</Nav.Link>
@@ -17,9 +19,10 @@ function NavbarMenu() {
             <Nav.Link href="#testimonials">Testimonials</Nav.Link>
             <Nav.Link href="#contact">Contact</Nav.Link>
           </Nav>
-        </RBNavbar.Collapse>
+        </Navbar.Collapse>
+
       </Container>
-    </RBNavbar>
+    </Navbar>
   );
 }
 
